ab19e6196ecf42ab3904a9695adb113aeede60d7
Brent Gardner <bgardner@squarelabs.net>
2016-05-18 09:51:15 -0600

    Cache connections

--- java/kudu-spark/src/main/scala/org/kududb/spark/kudu/ConnectionCache.scala
+++ java/kudu-spark/src/main/scala/org/kududb/spark/kudu/ConnectionCache.scala
@@ -6,7 +6,7 @@ import org.kududb.client.{AsyncKuduClient, KuduClient}
 
 object ConnectionCache {
   val syncCache = new ConcurrentHashMap[String, KuduClient]()
-  val asyncCache = new ConcurrentHashMap[String, KuduClient]()
+  val asyncCache = new ConcurrentHashMap[String, AsyncKuduClient]()
 
   def getSyncClient(kuduMaster: String): KuduClient = {
     val kuduMaker = new java.util.function.Function[String, KuduClient]() {
@@ -17,8 +17,8 @@ object ConnectionCache {
     syncCache.computeIfAbsent(kuduMaster, kuduMaker)
   }
 
-  def getAsyncClient(kuduMaster: String): KuduClient = {
-    val kuduMaker = new java.util.function.Function[String, KuduClient]() {
+  def getAsyncClient(kuduMaster: String): AsyncKuduClient = {
+    val kuduMaker = new java.util.function.Function[String, AsyncKuduClient]() {
       override def apply(kuduMaster: String): AsyncKuduClient = {
         new AsyncKuduClient.AsyncKuduClientBuilder(kuduMaster).build()
       }
